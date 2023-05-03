786eac9a7ad482846512540a6a9f7c7be834f315
Omar4589 <omar4589@hotmail.com>
2023-05-03 01:50:56 -0500

    updates post.js

--- public/js/post.js
+++ public/js/post.js
@@ -1,22 +1,25 @@
-$('#comment-form').on('submit', async function(event) {
-    event.preventDefault();
-  
-    const content = $('#content').val().trim();
-    const postId = window.location.pathname.split('/')[2];
-  
-    if (content) {
-      const response = await $.ajax({
-        url: `/api/blog/${postId}/comments`,
+$('#comment-form').on('submit', async function (event) {
+  event.preventDefault();
+
+  const content = $('#content').val().trim();
+  const postId = window.location.pathname.split('/')[2];
+
+  if (content) {
+    try {
+      const response = await fetch(`/api/blog/${postId}/comments`, {
         method: 'POST',
-        contentType: 'application/json',
-        data: JSON.stringify({ content }),
+        body: JSON.stringify({ content }),
+        headers: { 'Content-Type': 'application/json' },
       });
-  
-      if (response) {
+
+      if (response.ok) {
         location.reload();
       } else {
         alert('Failed to add comment');
       }
+    } catch (error) {
+      console.error(error);
+      alert('Failed to add comment');
     }
-  });
-  
+  }
+});
