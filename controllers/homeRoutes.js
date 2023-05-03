10264a57465790b1f7242f27486b609e888bb34f
Omar4589 <omar4589@hotmail.com>
2023-05-03 02:08:46 -0500

    fixes post.handlebars view to display post and comments

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -38,7 +38,7 @@ router.get("/post/:id", async (req, res) => {
     const post = postData.get({ plain: true });
 
     res.render("post", {
-      post,
+      ...post,
       loggedIn: req.session.loggedIn,
     });
   } catch (err) {
