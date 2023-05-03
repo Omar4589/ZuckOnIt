69e37d6feb42dae8ff2103f6286bebaea3dac4ac
Omar4589 <omar4589@hotmail.com>
2023-05-02 20:44:37 -0500

    creates blogpost api routes and moves js file into api folder

--- controllers/index.js
+++ controllers/index.js
@@ -2,10 +2,8 @@ const router = require("express").Router();
 
 const apiRoutes = require("./api");
 const homeRoutes = require("./homeRoutes");
-const blogRoutes = require("./blogRoutes");
 
 router.use("/", homeRoutes);
-router.use("/", blogRoutes);
 router.use("/api", apiRoutes);
 
 module.exports = router;
