21da95e67e5d6bfe3799360ab967754dfb853161
Omar4589 <omar4589@hotmail.com>
2023-05-02 21:06:34 -0500

    completes GET route for posts in homepage

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,7 +1,7 @@
-const router = require('express').Router();
-const { Post, User } = require('../models');
+const router = require("express").Router();
+const { Post, User, Comment } = require("../models");
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     const postData = await Post.findAll({
       include: [{ model: User }],
@@ -9,7 +9,7 @@ router.get('/', async (req, res) => {
 
     const posts = postData.map((post) => post.get({ plain: true }));
 
-    res.render('homepage', {
+    res.render("homepage", {
       posts,
       loggedIn: req.session.loggedIn,
     });
@@ -18,9 +18,33 @@ router.get('/', async (req, res) => {
   }
 });
 
-// router.get('/post/:id', async (req, res) => {
-   // Implement logic to retrieve a specific post and render the post view
-// });
+router.get("/post/:id", async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        { model: User },
+        {
+          model: Comment,
+          include: { model: User },
+        },
+      ],
+    });
+
+    if (!postData) {
+      res.status(404).json({ message: "No post found with this id" });
+      return;
+    }
+
+    const post = postData.get({ plain: true });
+
+    res.render("post", {
+      post,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // Add other route handlers as needed
 
